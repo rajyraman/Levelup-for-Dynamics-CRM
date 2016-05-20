79f8d000713acc986be4e7efaf565f062de09f19
Natraj Yegnaraman <rajyraman@gmail.com>
2016-05-20 22:37:08 +1000

    Removed bookmarklet file.

--- bookmarklet.js
+++
@@ -1,7 +0,0 @@
-var entityName = prompt("Entity?", ""), entityId = prompt("Id?", "");
-var url = Xrm.Page.context.getClientUrl() + '/main.aspx?etn=' + entityName + '&id=' + entityId + '&newWindow=true&pagetype=entityrecord';
-window.open(url, '_blank');
-chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
-    alert(msg);
-    alert(Xrm);
-});
