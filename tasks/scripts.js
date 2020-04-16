bfe99f67fca6d7a6939c603a8b7a229b0a09f0cb
Natraj Yegnaraman <natraj@dreamingincrm.com>
2020-04-16 16:26:03 +1000

    Fix library scripts and add GitHub Actions

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -11,61 +11,71 @@ import args from './lib/args';
 
 const ENV = args.production ? 'production' : 'development';
 
-gulp.task('scripts', (cb) => {
+gulp.task('libraries', (cb) => {
   return gulp
-    .src(['app/scripts/pages/*.ts', 'app/scripts/*.ts', 'app/libraries/*.js'])
-    .pipe(
-      plumber({
-        // Webpack will log the errors
-        errorHandler() {},
-      })
-    )
-    .pipe(named())
-    .pipe(
-      gulpWebpack(
-        {
-          devtool: args.sourcemaps ? 'inline-source-map' : false,
-          watch: args.watch,
-          output: {
-            filename: '[name].js',
-          },
-          mode: ENV,
-          plugins: [
-            new webpack.DefinePlugin({
-              'process.env.NODE_ENV': JSON.stringify(ENV),
-              'process.env.VENDOR': JSON.stringify(args.vendor),
-            }),
-          ].concat(args.production ? [new BabiliPlugin()] : []),
-          module: {
-            rules: [
-              {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                options: { logLevel: 'info', logInfoToStdOut: true },
-                exclude: /node_modules/,
-              },
-            ],
-          },
-          resolve: {
-            extensions: ['.ts', '.js'],
-            modules: ['node_modules/', 'app/scripts/'],
-          },
-        },
-        webpack,
-        (err, stats) => {
-          if (err) return;
-          log(
-            `Finished '${colors.cyan('scripts')}'`,
-            stats.toString({
-              chunks: false,
-              colors: true,
-              cached: false,
-              children: false,
-            })
-          );
-        }
-      )
-    )
+    .src(['app/libraries/*.js'])
     .pipe(gulp.dest(`dist/${args.vendor}/scripts`))
     .pipe(gulpif(args.watch, livereload()));
 });
+
+gulp.task(
+  'scripts',
+  gulp.series('libraries', () => {
+    return gulp
+      .src(['app/scripts/pages/*.ts', 'app/scripts/*.ts'])
+      .pipe(
+        plumber({
+          // Webpack will log the errors
+          errorHandler() {},
+        })
+      )
+      .pipe(named())
+      .pipe(
+        gulpWebpack(
+          {
+            devtool: args.sourcemaps ? 'inline-source-map' : false,
+            watch: args.watch,
+            output: {
+              filename: '[name].js',
+            },
+            mode: ENV,
+            plugins: [
+              new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(ENV),
+                'process.env.VENDOR': JSON.stringify(args.vendor),
+              }),
+            ].concat(args.production ? [new BabiliPlugin()] : []),
+            module: {
+              rules: [
+                {
+                  test: /\.ts$/,
+                  loader: 'ts-loader',
+                  options: { logLevel: 'info', logInfoToStdOut: true },
+                  exclude: /node_modules/,
+                },
+              ],
+            },
+            resolve: {
+              extensions: ['.ts', '.js'],
+              modules: ['node_modules/', 'app/scripts/'],
+            },
+          },
+          webpack,
+          (err, stats) => {
+            if (err) return;
+            log(
+              `Finished '${colors.cyan('scripts')}'`,
+              stats.toString({
+                chunks: false,
+                colors: true,
+                cached: false,
+                children: false,
+              })
+            );
+          }
+        )
+      )
+      .pipe(gulp.dest(`dist/${args.vendor}/scripts`))
+      .pipe(gulpif(args.watch, livereload()));
+  })
+);
